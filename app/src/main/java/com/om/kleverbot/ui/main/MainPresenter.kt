8ce96312366d9e5983de70c237d7d060254429af
Oday Maleh <oday.maleh@gmail.com>
2017-07-27 20:42:39 +0300

    Placing API key inside build.gradle

--- app/src/main/java/com/om/kleverbot/ui/main/MainPresenter.kt
+++ app/src/main/java/com/om/kleverbot/ui/main/MainPresenter.kt
@@ -1,12 +1,13 @@
 package com.om.kleverbot.ui.main
 
+import com.om.kleverbot.BuildConfig
 import com.om.kleverbot.api.ApiManager
 import com.om.kleverbot.ui.base.BasePresenterImpl
 
 class MainPresenter : BasePresenterImpl<MainContract.View>(), MainContract.Presenter {
 
   override fun talkToBot(input: String) {
-    compositeDisposable.add(ApiManager.talkToBot("CC3enJ_PpO4o01bcYzIWb3Q8zYA", input)
+    compositeDisposable.add(ApiManager.talkToBot(BuildConfig.CLEVERBOT_API_KEY, input)
         .subscribe({
           result ->
           view?.addMessageToView(result.output)
